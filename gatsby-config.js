4a862a7ec1844ccec54cc59d32cae8dcd89814f5
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-04-08 08:41:38 +0100

    add filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,14 @@
 module.exports = {
   /* Your site config here */
 plugins: [
-  'gatsby-plugin-sass'
+  'gatsby-plugin-sass',
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "files",
+      path: `${__dirname}/src/markdown`
+    },
+  },
 ]
 
 }

43e462006244d0ee0c06557dcb2ac38320c9d61a
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-04-09 17:27:56 +0100

    remove links

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,6 @@ import Title from "../components/title.js"
 export default () => (
 <Layout>
     <Title text="About Shaw"/>
-    
-    <div>
-    <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/blog">Blog</Link>
-    </div>
     <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate enim, voluptatum soluta voluptas nihil quidem animi ad facere! Odit nobis et atque dolor eligendi corporis consequatur repellat, necessitatibus veniam?
     </p>
